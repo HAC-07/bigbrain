c21ef62eae51ddf352d855733abb09a6c17633c2
HAC-07 <mr.himanshuchaudhari@gmail.com>
2025-05-19 10:44:07 +0530

    signup endpoint created

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,13 @@
-import mongoose, { Schema, Types } from "mongoose";
+import mongoose, { model, Schema, Types } from "mongoose";
+mongoose.connect("mongodb://localhost:27017/bigbrain")
 
 const UserSchema = new mongoose.Schema({
+  name: { type: String },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
-export const User = mongoose.model("User", UserSchema);
+export const Usermodel = model("User", UserSchema);
 
 const contenttype = ["X", "Youtube"];
 
@@ -13,14 +15,14 @@ const ContentSchema = new mongoose.Schema({
   type: { type: String, required: true },
   title: { type: String, enum: contenttype, required: true },
   link: { type: String, required: true },
-  userId: { type: Types.ObjectId, ref: User, required: true },
+  userId: { type: Types.ObjectId, ref: "User", required: true },
 });
 
-export const Content = mongoose.model("Content", ContentSchema);
+export const Contentmodel = model("Content", ContentSchema);
 
 const LinkSchema = new mongoose.Schema({
   hash: { type: String, required: true },
-  userId: { type: Types.ObjectId, ref: User, required: true },
+  userId: { type: Types.ObjectId, ref: "User", required: true },
 });
 
-export const Link = mongoose.model("Link", LinkSchema);
+export const Linkmodel = model("Link", LinkSchema);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,54 @@
 import express from "express";
+import { z } from "zod";
+import bcrypt from "bcrypt";
+import { Usermodel } from "./db";
 const app = express();
 
 app.use(express.json());
 
-app.post("/api/v1/signup", (req, res) => {
-    
+app.post("/api/v1/signup", async (req, res) => {
+  const ZodSchema = z.object({
+    name: z.string().min(1, "Name is Required"),
+    username: z
+      .string()
+      .min(3, "Username is Required with minimum length 3")
+      .max(18),
+    password: z.string().min(8, "Password with minimum lenth 8 is required"),
+  });
+
+  const Userdata = {
+    name: req.body.name,
+    username: req.body.username,
+    password: req.body.password,
+  };
+
+  const finaldata = ZodSchema.safeParse(Userdata);
+  if (finaldata.success) {
+    try {
+      const existingUser = await Usermodel.findOne({
+        username: Userdata.username,
+      });
+
+      if (existingUser) {
+        res.status(409).json({ message: "User already exists" });
+        return;
+      }
+
+      const hashedpassword = await bcrypt.hash(Userdata.password, 10);
+      await Usermodel.create({
+        name: finaldata.data.name,
+        username: finaldata.data.username,
+        password: hashedpassword,
+      });
+      res.status(200).json({
+        message: "User signed up",
+      });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Something went wrong. Please try again later." });
+    }
+  }
 });
 
 app.listen(3000);
